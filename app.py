5dda2d95e6d76912124914517c4230c12c6e4287
Geovane Filho <geo.vane.20002@gmail.com>
2023-11-20 22:51:36 -0300

    enable CORS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 import nltk
 from controllers.__all__controller import user_blueprint, production_blueprint, collection_blueprint
 
@@ -9,6 +10,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), 'nltk_data/tokeniz
     nltk.download('punkt', download_dir='nltk_data')
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(user_blueprint)
 app.register_blueprint(production_blueprint)
 app.register_blueprint(collection_blueprint)
